e8d2a91ff5eb2466fc34405670341cda24a420d4
adriancr1 <adrian.casal.rodriguez@gmail.com>
2022-09-13 15:30:45 +0200

    updated NFT contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x126f95B13fe4847e2E3c0c2a194Eec1B60813A69";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-nine-omega.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
